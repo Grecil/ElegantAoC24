102b4c6ae310d1b3ea918dd5761e11c0b1b4676b
Grecil <grecilunadkat@gmail.com>
2024-12-03 12:09:45 +0530

    optimisation

--- 03A.py
+++ 03A.py
@@ -2,5 +2,5 @@ import sys
 import re
 
 arr = sys.stdin.read()
-pattern = r"mul\(\s*(\d+)\s*,\s*(\d+)\s*\)"
+pattern = r"mul\((\d+),(\d+)\)"
 print(sum(int(x) * int(y) for x, y in re.findall(pattern, arr)))
