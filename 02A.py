ea5a223cca99a7fd7254f80827ad1d1add809bd5
Grecil <grecilunadkat@gmail.com>
2024-12-03 12:57:20 +0530

    optimisation

--- 02A.py
+++ 02A.py
@@ -3,11 +3,8 @@ import sys
 arr = [list(map(int, i.split())) for i in sys.stdin.read().split("\n")]
 print(
     sum(
-        (
-            all(i[j] > i[j + 1] for j in range(len(i) - 1))
-            or all(i[j] < i[j + 1] for j in range(len(i) - 1))
-        )
-        and all(0 < abs(i[j] - i[j + 1]) < 4 for j in range(len(i) - 1))
+        all(0 < i[j] - i[j + 1] < 4 for j in range(len(i) - 1))
+        or all(-4 < i[j] - i[j + 1] < 0 for j in range(len(i) - 1))
         for i in arr
     )
 )
--- 02B.py
+++ 02B.py
@@ -5,11 +5,8 @@ arr = [list(map(int, i.split())) for i in sys.stdin.read().split("\n")]
 print(
     sum(
         any(
-            (
-                all(i[j] > i[j + 1] for j in range(len(i) - 1))
-                or all(i[j] < i[j + 1] for j in range(len(i) - 1))
-            )
-            and all(0 < abs(i[j] - i[j + 1]) < 4 for j in range(len(i) - 1))
+            all(0 < i[j] - i[j + 1] < 4 for j in range(len(i) - 1))
+            or all(-4 < i[j] - i[j + 1] < 0 for j in range(len(i) - 1))
             for i in combinations(k, len(k) - 1)
         )
         for k in arr
