7f20e46d607a8f7ba722179fc8dade9be85099fa
Grecil <grecilunadkat@gmail.com>
2024-12-03 21:36:05 +0530

    optimisation

--- 03B.py
+++ 03B.py
@@ -2,15 +2,5 @@ import sys
 import re
 
 arr = sys.stdin.read()
-exp = r"mul\((\d+),(\d+)\)|(do\(\))|(don\'t\(\))"
-total_sum = 0
-matches = re.findall(exp, arr)
-state = True
-for j in matches:
-    if j[2] == "do()":
-        state = True
-    elif j[3] == "don't()":
-        state = False
-    elif state:
-        total_sum += int(j[0]) * int(j[1])
-print(total_sum)
+pattern = r"(?s)(?:(?<=don't\(\)).*?(?=do\(\)|$))|(?:mul\((\d+),(\d+)\))"
+print(sum(int(x) * int(y) if x and y else 0 for x, y in re.findall(pattern, arr)))
