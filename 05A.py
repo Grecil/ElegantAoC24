0271ae5642b90ebd2ed8f66b248be81b8c1378b2
Grecil <grecilunadkat@gmail.com>
2024-12-05 14:48:55 +0530

    Day 5

--- 05A.py
+++ 05A.py
@@ -3,8 +3,9 @@ import sys
 rules, updates = sys.stdin.read().split("\n\n")
 rules = [i.split("|") for i in rules.split("\n")]
 updates = [i.split(",") for i in updates.split("\n")]
-ans = 0
-for i in updates:
-    if not any(a in i and b in i and i.index(a) > i.index(b) for a, b in rules):
-        ans += int(i[len(i) // 2])
-print(ans)
+print(
+    sum(
+        int(i[len(i) // 2]) if not any(a in i and b in i and i.index(a) > i.index(b) for a, b in rules) else 0
+        for i in updates
+    )
+)
--- 05B.py
+++ 05B.py
@@ -3,10 +3,10 @@ from collections import defaultdict
 
 rules, updates = sys.stdin.read().split("\n\n")
 rules = [i.split("|") for i in rules.split("\n")]
+updates = [i.split(",") for i in updates.split("\n")]
 ruledict = defaultdict(set)
 for a, b in rules:
     ruledict[a].add(b)
-updates = [i.split(",") for i in updates.split("\n")]
 ans = 0
 for i in updates:
     if any(a in i and b in i and i.index(a) > i.index(b) for a, b in rules):
