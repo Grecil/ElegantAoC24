6e3c6827a70bee73323daaa420c8263db87bfde1
Grecil <grecilunadkat@gmail.com>
2024-12-07 16:12:52 +0530

    Day 6

---
+++ 06A.py
@@ -0,0 +1,21 @@
+import sys
+
+arr = [list(i) for i in sys.stdin.read().split("\n")]
+n, m = len(arr), len(arr[0])
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == "^":
+            x, y = i, j
+            break
+vis = set()
+dx, dy = -1, 0
+while True:
+    vis.add((x, y))
+    if not (-1 < x + dx < n and -1 < y + dy < m):
+        break
+    elif arr[x + dx][y + dy] == "#":
+        dx, dy = dy, -dx
+    else:
+        x += dx
+        y += dy
+print(len(vis))
---
+++ 06B.py
@@ -0,0 +1,32 @@
+import sys
+
+
+def search(x, y, dx, dy):
+    vis = set()
+    while True:
+        if (x, y, dx, dy) in vis:
+            return 1
+        vis.add((x, y, dx, dy))
+        if not (-1 < x + dx < n and -1 < y + dy < m):
+            return 0
+        elif arr[x + dx][y + dy] == "#":
+            dx, dy = dy, -dx
+            continue
+        x, y = x + dx, y + dy
+
+
+arr = [list(i) for i in sys.stdin.read().split("\n")]
+n, m = len(arr), len(arr[0])
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == "^":
+            x, y = i, j
+            break
+ans = 0
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] != "#":
+            arr[i][j] = "#"
+            ans += search(x, y, -1, 0)
+            arr[i][j] = "."
+print(ans)
