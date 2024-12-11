018889e64b2903d7dbf9e08d013e55f47f69ee9f
Grecil <grecilunadkat@gmail.com>
2024-12-11 15:09:45 +0530

    Day 11

---
+++ 11B.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+c = Counter(int(i) for i in input().split())
+
+for _ in range(75):
+    new = Counter()
+    for i in c:
+        if i == 0:
+            new[1] += c[i]
+        else:
+            s = str(i)
+            x = len(s)
+            if not x % 2:
+                new[int(s[x // 2 :])] += c[i]
+                new[int(s[: x // 2])] += c[i]
+            else:
+                new[i * 2024] += c[i]
+    c = new
+print(c.total())
