5feac9ed41c10b89703a858800d8c7addfe39d19
Grecil <grecilunadkat@gmail.com>
2024-12-14 13:32:26 +0530

    Day 14

--- 14B.py
+++ 14B.py
@@ -1,13 +1,12 @@
 import sys
 
 arr = [i.split() for i in sys.stdin.read().split("\n")]
-for i in range(10000):
+pos = [list(map(int, p[2:].split(","))) + list(map(int, v[2:].split(","))) for p, v in arr]
+for i in range(1, 10000):
     grid = [[" "] * 103 for _ in range(101)]
-    for p, v in arr:
-        px, py = map(int, p[2:].split(","))
-        vx, vy = map(int, v[2:].split(","))
-        x, y = (px + i * vx) % 101, (py + i * vy) % 103
-        grid[x][y] = "#"
+    for j in pos:
+        j[0], j[1] = (j[0] + j[2]) % 101, (j[1] + j[3]) % 103
+        grid[j[0]][j[1]] = "#"
     if any("#################################" in "".join(row) for row in grid):
         print(i)
         break
