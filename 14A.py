ffee19d4c4e746e6ca32bb9e4029d27a4c61ab12
Grecil <grecilunadkat@gmail.com>
2024-12-14 13:21:54 +0530

    Day 14

--- 14A.py
+++ 14A.py
@@ -8,5 +8,4 @@ for p, v in arr:
     x, y = (px + 100 * vx) % 101, (py + 100 * vy) % 103
     if x != 50 and y != 51:
         q[x > 50][y > 51] += 1
-print(q)
 print(q[0][0] * q[0][1] * q[1][0] * q[1][1])
