7d02a6c814475cdb1e543bb3fab17bb9979db447
Grecil <grecilunadkat@gmail.com>
2024-12-15 14:52:41 +0530

    Day 15

---
+++ 15A.py
@@ -0,0 +1,24 @@
+import sys
+
+arr, moves = sys.stdin.read().split("\n\n")
+moves = moves.replace("\n", "")
+grid = [list(i) for i in arr.split("\n")]
+n, m = len(grid), len(grid[0])
+for i in range(1, n - 1):
+    for j in range(1, m - 1):
+        if grid[i][j] == "@":
+            x, y = i, j
+            break
+d = {"<": (0, -1), ">": (0, 1), "^": (-1, 0), "v": (1, 0)}
+for move in moves:
+    dx, dy = d[move]
+    nx, ny = x + dx, y + dy
+    while grid[nx][ny] != "#":
+        if grid[nx][ny] == ".":
+            while nx != x or ny != y:
+                grid[nx][ny], grid[nx - dx][ny - dy] = grid[nx - dx][ny - dy], grid[nx][ny]
+                nx, ny = nx - dx, ny - dy
+            x, y = x + dx, y + dy
+            break
+        nx, ny = nx + dx, ny + dy
+print(sum(100 * i + j if grid[i][j] == "O" else 0 for i in range(1, n - 1) for j in range(1, m - 1)))
---
+++ 15B.py
@@ -0,0 +1,32 @@
+import sys
+
+arr, moves = sys.stdin.read().split("\n\n")
+moves = moves.replace("\n", "")
+arr = arr.replace("#", "##").replace(".", "..").replace("O", "[]").replace("@", "@.")
+grid = [list(i) for i in arr.split("\n")]
+n, m = len(grid), len(grid[0])
+for i in range(1, n - 1):
+    for j in range(1, m - 1):
+        if grid[i][j] == "@":
+            x, y = i, j
+            break
+d = {"<": (0, -1), ">": (0, 1), "^": (-1, 0), "v": (1, 0)}
+for move in moves:
+    dx, dy = d[move]
+    to_move = [(x, y)]
+    flag = True
+    for i, j in to_move:
+        nx, ny = i + dx, j + dy
+        if (nx, ny) not in to_move:
+            if grid[nx][ny] == "#":
+                flag = False
+                break
+            elif grid[nx][ny] == "[":
+                to_move.extend([(nx, ny), (nx, ny + 1)])
+            if grid[nx][ny] == "]":
+                to_move.extend([(nx, ny), (nx, ny - 1)])
+    if flag:
+        for i, j in to_move[::-1]:
+            grid[i + dx][j + dy], grid[i][j] = grid[i][j], grid[i + dx][j + dy]
+        x, y = x + dx, y + dy
+print(sum(100 * i + j if grid[i][j] == "[" else 0 for i in range(1, n - 1) for j in range(1, m - 1)))
