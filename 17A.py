af0698f0281e6bff8aca00af332f5a53fd1def3b
Grecil <grecilunadkat@gmail.com>
2024-12-18 00:30:39 +0530

    Day 17

--- 17A.py
+++ 17A.py
@@ -1,13 +1,11 @@
-def combo(x):
-    d = {4: A, 5: B, 6: C}
-    return x if x < 4 else d[x]
-
-
 A = int(input()[12:])
 B = int(input()[12:])
 C = int(input()[12:])
 input()
 arr = [int(i) for i in input()[9:].split(",")]
+
+combo = lambda x: x if x < 4 else {4: A, 5: B, 6: C}[x]
+
 i, n = 0, len(arr)
 ans = []
 while i < n - 1:
--- 17B.py
+++ 17B.py
@@ -3,15 +3,33 @@ B = int(input()[12:])
 C = int(input()[12:])
 input()
 arr = [int(i) for i in input()[9:].split(",")]
+n = len(arr)
 
 
-# hardcoded for my input
 def run(a):
+    combo = lambda x: x if x < 4 else {4: a, 5: b, 6: c}[x]
+    i = b = c = 0
     out = []
-    while a:
-        b = a % 8 ^ 3
-        out.append(b ^ 5 ^ (a >> b) % 8)
-        a //= 8
+    while i < n - 1:
+        code, op = arr[i], arr[i + 1]
+        i += 2
+        match code:
+            case 0:
+                a //= pow(2, combo(op))
+            case 1:
+                b ^= op
+            case 2:
+                b = combo(op) % 8
+            case 3:
+                i = op if a != 0 else i
+            case 4:
+                b ^= c
+            case 5:
+                out.append(combo(op) % 8)
+            case 6:
+                b = a // pow(2, combo(op))
+            case 7:
+                c = a // pow(2, combo(op))
     return out
 
 
